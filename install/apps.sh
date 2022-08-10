a670e818535da49749bf9d502acc9aa83c4e7d83
Joanne Yeung <jttyeung@gmail.com>
2022-08-10 10:20:40 -0600

    Update apps & tools

--- install/apps.sh
+++ install/apps.sh
@@ -1,20 +1,13 @@
 # homebrew stuff
 ok brew
-ok brew-tap caskroom/cask
-ok brew mas
 
 # work
 ok cask docker
 ok cask iterm2
 
-# security and backups
-ok cask 1password
+# backups
 ok cask dropbox
 
 # misc
-ok cask filezilla
 ok cask rectangle
 ok cask spotify
-
-# appstore
-ok mas 497799835 Xcode
--- install/tools.sh
+++ install/tools.sh
@@ -5,11 +5,13 @@ ok brew dep
 ok brew git
 ok brew git-secrets
 ok brew grip
+ok brew tap heroku/brew
 ok brew heroku
 ok brew kubectx
 ok brew kubernetes-cli
 ok brew libyubikey
 ok brew postgresql
+ok brew romkatv/powerlevel10k/powerlevel10k
 ok brew the_silver_searcher
 ok brew tmate
 ok brew tmux

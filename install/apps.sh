4f81d9c687bc65619f85bc0bfbc33f1c0d90c297
Joanne Yeung <jttyeung@gmail.com>
2023-10-30 13:50:28 -0600

    Update casks

--- install/apps.sh
+++ install/apps.sh
@@ -1,13 +1,13 @@
 # homebrew stuff
 ok brew
 
-# work
-ok cask docker
-ok cask iterm2
-
-# backups
+# personal stuff
+ok cask 1password
 ok cask dropbox
-
-# misc
 ok cask rectangle
 ok cask spotify
+
+# werk stuff
+ok cask git-credential-manager-core
+ok cask iterm2
+ok cask docker
